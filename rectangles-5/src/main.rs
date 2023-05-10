0e10d14f87936a291e07041f30678193f2209e60
Jack <jdburton0@outlook.com>
2023-05-10 19:10:56 -0400

    Finish chapter 5

--- rectangles-5/src/main.rs
+++ rectangles-5/src/main.rs
@@ -1,15 +1,53 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
+#[derive(Debug)]
+struct Rectangle {
+    width: usize,
+    height: usize,
+}
 
-    let string = String::from("Hello");
+impl Rectangle {
+    fn area(&self) -> usize {
+        self.height * self.width
+    }
+
+    fn square(size: usize) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+fn main() {
+    let rect1 = (30, 50);
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area_tuple(rect1)
+    );
+    println!("Rectangle struct is {:#?}", rect2);
+    dbg!(&rect2);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect2)
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect2.area()
+    );
+    let square1 = Rectangle::square(45);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        square1.area()
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area_tuple(dimensions: (usize, usize)) -> usize {
+    dimensions.0 * dimensions.1
+}
+
+fn area_struct(rect: &Rectangle) -> usize {
+    rect.width * rect.height
 }
