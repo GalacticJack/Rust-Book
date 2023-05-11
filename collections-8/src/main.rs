a2da84ab3ebcee01c068a14f8f07762829a31fc9
Jack Burton <jackson.burton@feddata.com>
2023-05-11 10:08:00 -0400

    Finish chapter 8

--- collections-8/src/main.rs
+++ collections-8/src/main.rs
@@ -1,6 +1,10 @@
-#![allow(unused_variables)]
+#![allow(unused_variables, dead_code, unused_mut)]
+
+use std::collections::HashMap;
 fn main() {
     vectors();
+    strings();
+    hashmaps();
 }
 
 fn vectors() {
@@ -42,4 +46,85 @@ fn vectors() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+}
+
+fn strings() {
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+
+    let s = "initial contents".to_string();
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("{}", s);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("{}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    println!("{}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{s}");
+
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
+}
+
+fn hashmaps() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:?}", scores);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("{score}");
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
