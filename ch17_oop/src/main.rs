e864e8057d2446a80b071a4e6ec19ecda79b306c
Jack Burton <jackson.burton@feddata.com>
2023-05-23 10:43:21 -0400

    Finished chapter 17

---
+++ ch17_oop/src/main.rs
@@ -0,0 +1,54 @@
+#![allow(dead_code)]
+use ch17_oop::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+fn screen_draw() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+pub mod blog;
+use crate::blog::Post;
+fn main() {
+    screen_draw();
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
